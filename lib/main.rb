d446b94c19bd79cb30b39ad7ccc6165419049e70
sunil <sunil@sunil-System-Product-Name>
2018-04-09 17:14:10 +0530

    BastardsBookOFRuby

---
+++ lib/main.rb
@@ -0,0 +1,24 @@
+
+
+require 'open-uri'
+def just_fetch_page(url)
+  return open(url).read
+end
+
+def just_count_tags(page, tag)
+   pattern = /<#{tag}\b/
+   tags = page.scan(pattern)
+   return tags.length
+end
+
+sites = [ "https://www.wsj.com", "https://www.nytimes.com", "https://www.ft.com" ]
+tags = ["div","h1","h2","h3","img","p"]   
+
+sites.each do |url|
+   puts "#{url} has:"
+   tags.each do |tag|
+      page = just_fetch_page(url)
+      tag_count = just_count_tags(page, tag)
+      puts "\t - #{tag_count} <#{tag}> tags"
+   end
+end
